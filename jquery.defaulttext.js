f26e90d3c513516ab40411df6472cb6608ca9bd6
Karl Swedberg <karl@englishrules.com>
2009-06-02 10:48:01 -0400

    re-factored plugin to use artificial event binding.

--- jquery.defaulttext.js
+++ jquery.defaulttext.js
@@ -4,40 +4,65 @@
 * @version 1.0 (03/30/2009)
 * @requires jQuery v1.3+ 
 ************************************** */
- 
-(function($) {
+
+
+
+(function($){
+		
   $.fn.defaulttext = function(options) {
     
     var elText = { 
-      title: function(input) {
-        return $(input).attr('title');
-      },
-      label: function(input) {
-        return $('label[for=' + input.id +']').text();
-      }
-    },
-    defaultText,
-    selector = this.selector;
+          title: function(input) {
+            return $(input).attr('title');
+          },
+          label: function(input) {
+            return $('label[for=' + input.id +']').text();
+          }
+        },
+        defaultText,
+        selector = this.selector,
+        $form = this.parents('form:first');
 
-    return this.each(function() {
+    $form
+    .bind('blurText.dt', function(event) {
+      var $tgt = $(event.target),
+          prevClass = $tgt.data('dtInfo').prevClass;
+      if ($.trim($tgt.val()) === '') {
+        $tgt.prev(prevClass).show();
+      } else {
+        $tgt.prev(prevClass).hide();
+      }
+    })
+    .bind('focusText.dt', function(event, el) {
+      var tgt = event.target;
+      $(tgt).prev().hide();
+      if (!$(el).is(':dtinput')) {
+        tgt.focus();
+      } 
+    });
+    
+    this.filter(':dtinput').each(function() {
       var $input = $(this);
-      if (this.type === 'text' || this.nodeName.toLowerCase() === 'textarea') {
         var opts = $.extend({}, $.fn.defaulttext.defaults, options || {}, $.metadata ? $input.metadata() : $.meta ? $input.data() : {});
         // set the default text based on the value of the text option
         if (opts.text.constructor === Function) {
-          defaultText = opts.text(this);
+          $input.data('defaultText', opts.text(this));
         } else if (opts.text && opts.text.constructor === String){
-          defaultText = (/(title|label)/).test(opts.text) ? elText[opts.text](this) : opts.text;
+          $input.data('dtInfo', {
+            text: ((/(title|label)/).test(opts.text) ? elText[opts.text](this) : opts.text)
+          });
           if (opts.text === 'label') {
             $('label[for= '+ this.id + ']').css({position: 'absolute', left: '-4000em'});
           }
         } 
-        if (!defaultText) {return;}
-
+        if (!$input.data('dtInfo').text) {return $input.removeData('dtInfo');}
+        
+        $input.data('dtinfo', {prevClass: opts.defaultClass ? '.' + opts.defaultClass : ''});
+        
         if ($input.parent().css('position') == 'static') {
           $input.parent().css({position: 'relative'});
         }
-        $(opts.tag).html(defaultText)
+        $(opts.tag).html($input.data('dtInfo').text)
           .addClass(opts.defaultClass)
           .css({
             position: 'absolute',
@@ -48,39 +73,43 @@
           .insertBefore($input);
 
         // hide default text on focus
-        $input.focus(function() {
-          dtHide($input);
+        $input.bind('focus', function(event) {
+          $input.trigger('focusText.dt', event.target);
         });
-        var prevClass = opts.defaultClass ? '.' + opts.defaultClass : '';
-        $input.prev(prevClass).click(function() {
-          dtHide($input);
-          $input.focus();
+        $input.prev($input.data('dtInfo').prevClass).bind('click', function(event) {
+          $input.trigger('focusText.dt', event.target);
         });
-
-        // conditionally show default text on ready and input blur
         
-        $input.bind('blur', function(event, init) {
-          if ($.trim($(this).val()) == '') {
-            $(this).prev(prevClass).show();
-          } else {
-            $(this).prev(prevClass).hide();
-          }
-          if (!init) {
-            $(selector).not($input).triggerHandler('blur');
-          }
-        })
-        .triggerHandler('blur', true);
-      }
+        // conditionally show default text on input blur
+        $input.bind('blur', function(event) {
+          $(this).trigger('blurText.dt');
+        });
+        
+        // trigger the focs and blur when the window has loaded
+        $(window).bind('load', function() {
+          $input.trigger('focusText.dt', $input);
+          $input.trigger('blurText.dt');
+        });
     });
 
     function dtHide(el) {
       el.prev().hide();
     }
+    return this;
   };
+
   $.fn.defaulttext.defaults = {
     tag: '<span></span>',
     defaultClass: 'default-text',
     text: 'label'            // 'label' uses text of input's label; 'title' uses input's title attribute. 
                               //  otherwise, use some other string or function
   };
+
+
+  $.extend($.expr[':'], {
+    dtinput: function(element, index, matches, set) {
+      return (/text|password/i).test(element.type) || element.nodeName.toLowerCase() === 'textarea';
+    }
+  });
+  
 })(jQuery);
